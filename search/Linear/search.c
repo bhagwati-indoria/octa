896f52e172eed41a5cb9cd7fb77fca264c24cf4a
my-tests <bhagwati@thekha.in>
2017-05-11 02:20:13 +0530

    search, liner => Added array read and array print functions

--- search/Linear/search.c
+++ search/Linear/search.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void readArr(int * arr, int arrLen);
 void printArr(int * arr);
-void readArr(int * arr, int N);
 
 int main(int argc, const char * argv[]) {
     int N, M;
     int arr[100000];
     scanf("%d %d", &N, &M);
     readArr(arr, N);
-printArr(arr);
+    printArr(arr);
     return EXIT_SUCCESS;
 }
 
-void printArr(int arr[]) {
-    int i = 0;
-    while(arr[i] != '\0') {
-        printf("Element at position %d is %d\n", i+1, arr[i]);
-        i++;
+
+void readArr(int * arr, int arrLen) {
+    int i;
+    for(i=0; i<arrLen; i++) {
+        scanf("%d", &arr[i]);
     }
 }
 
-void readArr(int arr[], int N) {
-    int i;
-    for(i=0; i<N; i++) {
-        scanf("%d", &arr[i]);
+void printArr(int * arr) {
+    int i = 0;
+    while(arr[i] != '\0') {
+        printf("arr[%d] = %d\n", i, arr[i]);
+        i++;
     }
 }
